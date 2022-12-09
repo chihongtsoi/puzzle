8e6592f29e88c6f91c49675516ac3231a49876f1
chihongtsoi <chihongtsoi@gmail.com>
2022-12-09 19:46:45 +0800

    AOC 20221209

--- src/aoc/dec2022/Q20221208.java
+++ src/aoc/dec2022/Q20221208.java
@@ -5,6 +5,7 @@ import objectprinter.oj.BaseSolution;
 import objectprinter.oj.Run;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
@@ -16,28 +17,23 @@ public class Q20221208 extends BaseSolution {
         int[][] matrix = resolveInput(s);
         int m = matrix.length;
         int n = matrix[0].length;
-        int[][][] dp = new int[4][m + 2][n + 2];
+        boolean[][] counted = new boolean[m][n];
         WatchDirection[] directions = WatchDirection.values();
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                for (WatchDirection d : directions) {
+        int count =0;
+        for (WatchDirection d : directions) {
+            int[] highest = new int[d.getK(m,n)];
+            Arrays.fill(highest, -1);
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
                     int _i = d.getI(i, m);
                     int _j = d.getJ(j, n);
-                    dp[d.ordinal()][_i][_j] = Math.max(
-                            matrix[_i - 1][_j - 1],
-                            dp[d.ordinal()][_i + d.x][_j + d.y]
-                    );
-                }
-            }
-        }
-        int count = (m + n - 2) * 2;
-        for (int i = 2; i < m; i++) {
-            for (int j = 2; j < n; j++) {
-                for (WatchDirection d : WatchDirection.values()) {
-                    if (matrix[i - 1][j - 1] > dp[d.ordinal()][i + d.x][j + d.y]) {
+                    int _k = d.getK(i,j);
+                    int cur = matrix[_i][_j];
+                    if(!counted[_i][_j] &&cur>highest[_k]){
                         count++;
-                        break;
+                        counted[_i][_j] = true;
                     }
+                    highest[_k] = Math.max(highest[_k], cur);
                 }
             }
         }
@@ -50,38 +46,33 @@ public class Q20221208 extends BaseSolution {
         int[][] matrix = resolveInput(s);
         int m = matrix.length;
         int n = matrix[0].length;
-        long max = 0;
         List<List<Stack<int[]>>> stacks = new ArrayList<>();
         for (int i = 0; i < 4; i++) stacks.add(new Stack<>());
         WatchDirection[] directions = WatchDirection.values();
-        int[][][] dp = new int[4][m + 2][n + 2];
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
+        int[][] multiply = new int[m][n];
+        for (int i = 0; i < m; i++) Arrays.fill(multiply[i], 1);
+        int max = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 for (WatchDirection d : directions) {
                     int _i = d.getI(i, m);
                     int _j = d.getJ(j, n);
-                    int _k = d.getK(i, j) - 1;
+                    int _k = d.getK(i, j);
                     List<Stack<int[]>> rcStack = stacks.get(d.ordinal());
                     if (_k >= rcStack.size()) rcStack.add(new Stack<>());
                     Stack<int[]> monoStack = rcStack.get(_k);
-                    int cur = matrix[_i - 1][_j - 1];
+                    int cur = matrix[_i][_j];
                     if (monoStack.isEmpty()) {
                         monoStack.push(new int[]{cur, 1});
                         continue;
                     }
                     int count = 0;
                     while (!monoStack.isEmpty() && cur > monoStack.peek()[0]) count += monoStack.pop()[1];
-                    dp[d.ordinal()][_i][_j] = count + (monoStack.isEmpty() ? 0 : 1);
+                    multiply[_i][_j] *= count + (monoStack.isEmpty() ? 0 : 1);
+                    max = Math.max(max, multiply[_i][_j]);
                     if (!monoStack.isEmpty() && monoStack.peek()[0] == cur) count += monoStack.pop()[1];
                     monoStack.push(new int[]{cur, count + 1});
                 }
-            }
-        }
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                int score = 1;
-                for (WatchDirection d : directions) score *= dp[d.ordinal()][i][j];
-                max = Math.max(max, score);
             }
         }
         return max;
@@ -112,14 +103,14 @@ public class Q20221208 extends BaseSolution {
                 //start with top left corner
                 case EAST, SOUTH -> i;
                 //start with bottom right corner
-                case WEST, NORTH -> m - i + 1;
+                case WEST, NORTH -> m - i - 1;
             };
         }
 
         public int getJ(int j, int n) {
             return switch (this) {
                 case EAST, SOUTH -> j;
-                case WEST, NORTH -> n - j + 1;
+                case WEST, NORTH -> n - j -1;
             };
         }
 
